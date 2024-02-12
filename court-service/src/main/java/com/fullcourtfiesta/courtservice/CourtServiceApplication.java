20f6a16e6ae61160a21203f19b61176f01a89e09
sergiogurillo <sergiogurillo@joinhealz.com>
2024-02-12 13:22:55 +0100

    add swagger to court service

--- court-service/src/main/java/com/fullcourtfiesta/courtservice/CourtServiceApplication.java
+++ court-service/src/main/java/com/fullcourtfiesta/courtservice/CourtServiceApplication.java
@@ -1,11 +1,14 @@
 package com.fullcourtfiesta.courtservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableWebFlux
+@OpenAPIDefinition(info = @Info(title = "Court Service Backend", version = "1.0", description = "Documentation Court Service v1.0"))
 public class CourtServiceApplication {
 
   public static void main(String[] args) {
--- court-service/src/main/java/com/fullcourtfiesta/courtservice/infrastructure/delivery/controller/CourtController.java
+++ court-service/src/main/java/com/fullcourtfiesta/courtservice/infrastructure/delivery/controller/CourtController.java
@@ -3,6 +3,8 @@ package com.fullcourtfiesta.courtservice.infrastructure.delivery.controller;
 import com.fullcourtfiesta.courtservice.application.CourtService;
 import com.fullcourtfiesta.courtservice.domain.CourtRq;
 import com.fullcourtfiesta.courtservice.domain.CourtRs;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,28 +19,53 @@ public class CourtController {
   private final CourtService courtService;
 
   @GetMapping
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "OK"),
+      @ApiResponse(responseCode = "500", description = "Internal server error")
+  })
   public Flux<CourtRs> all() {
     return courtService.all();
   }
 
   @GetMapping("/{id}")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "OK"),
+      @ApiResponse(responseCode = "404", description = "Item not found"),
+      @ApiResponse(responseCode = "500", description = "Internal server error")
+  })
   public Mono<CourtRs> one(@PathVariable("id") final Long id) {
     return courtService.one(id);
   }
 
   @PostMapping
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "OK"),
+      @ApiResponse(responseCode = "401", description = "BAD REQUEST"),
+      @ApiResponse(responseCode = "500", description = "Internal server error")
+  })
   public Mono<CourtRs> create(@RequestBody final CourtRq court) {
     return courtService.create(court);
   }
 
   @PutMapping("/{id}")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "OK"),
+      @ApiResponse(responseCode = "401", description = "BAD REQUEST"),
+      @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+      @ApiResponse(responseCode = "500", description = "Internal server error")
+  })
   public Mono<CourtRs> replace(@RequestBody final CourtRq court, @PathVariable("id") final Long id) {
     return courtService.update(court, id);
   }
 
   @DeleteMapping("/{id}")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "OK"),
+      @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+      @ApiResponse(responseCode = "500", description = "Internal server error")
+  })
   public Mono<ResponseEntity<Void>> delete(@PathVariable("id") final Long id) {
     return courtService.delete(id)
-        .then(Mono.just(ResponseEntity.noContent().<Void>build()));
+        .then(Mono.just(ResponseEntity.noContent().build()));
   }
 }
